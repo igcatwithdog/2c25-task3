d5d28113171d66319859a878387beb9a8afaf040
igcatwithdog <115450419+igcatwithdog@users.noreply.github.com>
2022-10-10 15:00:26 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,58 @@
-basic.showNumber(1)
-basic.showNumber(2)
-basic.showNumber(3)
-basic.showNumber(4)
-basic.showNumber(5)
-basic.showNumber(6)
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(1)
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showNumber(4)
+    basic.showNumber(5)
+    basic.showNumber(6)
+    basic.showNumber(0)
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
 basic.forever(function () {
 	
 })
