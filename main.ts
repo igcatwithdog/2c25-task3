2b75918130da8feaef09f6122d67d37314fe8854
igcatwithdog <115450419+igcatwithdog@users.noreply.github.com>
2022-10-17 14:39:28 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,17 @@
+let count = 0
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(randint(1, 6))
 })
+input.onButtonPressed(Button.AB, function () {
+    count = 0
+})
 input.onButtonPressed(Button.B, function () {
-    basic.showNumber(9)
-    basic.showNumber(8)
-    basic.showNumber(7)
-    basic.showNumber(6)
-    basic.showNumber(5)
-    basic.showNumber(4)
-    basic.showNumber(3)
-    basic.showNumber(2)
-    basic.showNumber(1)
-    basic.showNumber(0)
-    basic.pause(2000)
+    count = 9
+    basic.showNumber(count)
+    while (count > 0) {
+        count += -1
+        basic.showNumber(count)
+    }
     basic.showLeds(`
         . . . . .
         . . . . .
