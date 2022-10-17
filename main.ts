31b08a31895b09d271b6437df34192da4a3cd01c
igcatwithdog <115450419+igcatwithdog@users.noreply.github.com>
2022-10-17 14:14:49 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(1)
-    basic.showNumber(2)
-    basic.showNumber(3)
-    basic.showNumber(4)
-    basic.showNumber(5)
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(9)
+    basic.showNumber(8)
+    basic.showNumber(7)
     basic.showNumber(6)
+    basic.showNumber(5)
+    basic.showNumber(4)
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
     basic.showNumber(0)
     basic.pause(2000)
     basic.showLeds(`
@@ -49,10 +55,4 @@ input.onButtonPressed(Button.A, function () {
         . . . . .
         . . . . .
         `)
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
-basic.forever(function () {
-	
 })
